9549a58324712b3065a0079208761a0c2cf33c44
anybody <anybody@anywhere.any>
2015-03-11 06:07:34 +0600

    trimmed track number

--- app/Program.fs
+++ app/Program.fs
@@ -1,7 +1,20 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System.IO
+
+let fileReader file = seq {
+    use reader = new System.IO.StreamReader(File.OpenRead(file))
+    while not reader.EndOfStream do
+        yield reader.ReadLine() }  
+ 
+let skipTrackNumber (str: string) = 
+    str.TrimStart("1234567890 ".ToCharArray())
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let strings = fileReader "songs.txt"
+    let removedTrackNum = strings |> Seq.map skipTrackNumber
+    printfn "%A" removedTrackNum
     0 // return an integer exit code
+            
